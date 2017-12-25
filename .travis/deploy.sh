9556ee6aa64ee19e7a4efbd055e1cfd9adb059f4
Markus Fröschle <mfro@mubf.de>
2017-12-25 18:02:04 +0100

    add travis-ci control files

---
+++ .travis/deploy.sh
@@ -0,0 +1,5 @@
+#!/bin/bash -eu
+# -e: Exit immediately if a command exits with a non-zero status.
+# -u: Treat unset variables as an error when substituting.
+
+echo "no deployment step implemented (yet)"
