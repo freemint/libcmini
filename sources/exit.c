36dcaf416cd800ffd0c50b0c22762e0bfcd44414
Markus Fröschle <mfro@mubf.de>
2018-02-24 20:46:37 +0100

    fix formatting

--- sources/exit.c
+++ sources/exit.c
@@ -16,21 +16,20 @@ extern FILE *__stdio_head;
 
 void exit(int status)
 {
-	
-	/* fist: call all atexit-registered func's */
-	for(;_num_at_exit; --_num_at_exit)
-		(*_at_exit++) ();
-	
-	/* second: close all files */
-	FILE *stream = __stdio_head;
-	while (stream) {  
-		FILE *stream_next = __stdio_head->__next;
-		if (stream->__magic == _IOMAGIC)
-			fclose (stream);
-		stream = stream_next;
-	}
-	/* third: exit*/
-	(void) Pterm(status);
-	while(1);	/* get rid of gcc complaining about returning from exit() */
+    /* first: call all atexit-registered func's */
+    for(; _num_at_exit; --_num_at_exit)
+        (*_at_exit++) ();
+
+    /* second: close all files */
+    FILE *stream = __stdio_head;
+    while (stream) {
+        FILE *stream_next = __stdio_head->__next;
+        if (stream->__magic == _IOMAGIC)
+            fclose (stream);
+        stream = stream_next;
+    }
+    /* third: exit*/
+    (void) Pterm(status);
+    while(1);	/* get rid of gcc complaining about returning from exit() */
 }
 
