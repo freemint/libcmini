2c2b33b4815acf8fe421fdbd245b32dd46660c93
Thorsten Otto <admin@tho-otto.de>
2018-06-22 23:07:12 +0200

    Provide minimalistic version of <errno.h>

---
+++ include/errno.h
@@ -0,0 +1,16 @@
+extern int errno;
+
+#define ENODEV  15
+#define EINVAL  25
+#define ENOMEM  39
+#define EFAULT  40
+#define EBADF   25
+#define ENOTDIR 34
+#define ENOENT  33
+#define ENOSYS  32
+#define ELOOP   80
+#define EISDIR  24
+#define EACCES  36
+#define EEXIST  85
+#define ERANGE  88
+#define EDOM    89
