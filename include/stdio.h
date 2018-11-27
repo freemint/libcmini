5b3e453a686a8561992c65e3598a52fa115baa93
Markus Fröschle <mfro@mubf.de>
2018-11-27 20:14:05 +0100

    Revert "add _Isbit() macro"
    This reverts commit 0b511209a873ed69222512cd46404fa4b31ef4fb.

--- include/stdio.h
+++ include/stdio.h
@@ -1,9 +1,9 @@
 /*
-* stdio.h
-*
-*  Created on: 29.05.2013
-*      Author: mfro
-*/
+ * stdio.h
+ *
+ *  Created on: 29.05.2013
+ *      Author: mfro
+ */
 
 #ifndef _STDIO_H_
 #define _STDIO_H_
@@ -15,38 +15,38 @@
 typedef struct __stdio_file FILE;
 typedef struct __stdio_file
 {
-   int __magic;
+    int __magic;
 #define	_IOMAGIC ((int) 0xfedabeeb)	/* Magic number to fill `__magic'.  */
 
 #if 0
-   void *BufPtr;     /* next byte write */
-   void *BufLvl;     /* next byte read */
-   void *BufStart;   /* first byte of buffer */
-   void *BufEnd;     /* first byte after buffer */
+    void *BufPtr;     /* next byte write */
+    void *BufLvl;     /* next byte read */
+    void *BufStart;   /* first byte of buffer */
+    void *BufEnd;     /* first byte after buffer */
 #endif
-   long Handle;      /* GEMDOS handle */
+    long Handle;      /* GEMDOS handle */
 #if 0
-   char Flags;
-   char resv;
-   char ChrBuf;
-   char ungetFlag;
+    char Flags;
+    char resv;
+    char ChrBuf;
+    char ungetFlag;
 #endif
-   FILE *__next;     /* Next FILE in the linked list.  */
+    FILE *__next;     /* Next FILE in the linked list.  */
 
 } FILE;
 
-FILE *stdout;
-FILE *stdin;
-FILE *stderr;
+extern FILE *stdout;
+extern FILE *stdin;
+extern FILE *stderr;
 
 #define stdaux	(&_StdAuxF)
 #define stdprn	(&_StdPrnF)
 
-FILE _StdOutF;
-FILE _StdInF;
-FILE _StdErrF;
-FILE _StdAuxF;
-FILE _StdPrnF;
+extern FILE _StdOutF;
+extern FILE _StdInF;
+extern FILE _StdErrF;
+extern FILE _StdAuxF;
+extern FILE _StdPrnF;
 
 #define _IONBF	2
 
@@ -62,8 +62,8 @@ FILE _StdPrnF;
 #define EOF             (-1)
 
 /*
-* access modes for open()
-*/
+ * access modes for open()
+ */
 #define O_RDONLY        0x00    /* open read-only */
 #define O_WRONLY        0x01    /* open write-only (this doesn't work apparently on _all_ TOS versions */
 #define O_RDWR          0x02    /* open for reading and writing */
@@ -76,42 +76,42 @@ FILE _StdPrnF;
 #define SEEK_CUR    1   /* Seek from current position.  */
 #define SEEK_END    2   /* Seek from end of file.  */
 
-int errno;
+extern int errno;
 
-FILE *fopen(const char *path, const char *mode);
-int fclose(FILE *fp);
-size_t fread(void *ptr, size_t size, size_t nmemb, FILE *stream);
-size_t fwrite(const void *ptr, size_t size, size_t nmemb, FILE *stream);
-int fseek(FILE *fp, long offset, int origin);
-long ftell(FILE *stream);
-
-
+extern FILE *fopen(const char *path, const char *mode);
+extern int fclose(FILE *fp);
+extern size_t fread(void *ptr, size_t size, size_t nmemb, FILE *stream);
+extern size_t fwrite(const void *ptr, size_t size, size_t nmemb, FILE *stream);
+extern int fseek(FILE *fp, long offset, int origin);
+extern long ftell(FILE *stream);
 
 
-int fputs(const char *s, FILE *stream);
-int puts(const char *s);
-int fputc(int c, FILE *stream);
-int putc(int c, FILE *stream);
 
-int scanf(const char *format, ...);
-int fscanf(FILE *stream, const char *format, ...);
-int fgetc(FILE *stream);
-int putchar(int c);
-int fprintf(FILE *stream, const char *format, ...);
-int vfprintf(FILE *stream, const char *format, va_list ap);
 
-int printf(const char *fmt, ...);
-int snprintf(char *s, size_t size, const char *fmt, ...);
-int vsnprintf(char *str, size_t size, const char *fmt, va_list va);
-int sprintf(char *s, const char *format, ...);
-int vsprintf(char *s, const char *format, va_list va);
+extern int fputs(const char *s, FILE *stream);
+extern int puts(const char *s);
+extern int fputc(int c, FILE *stream);
+extern int putc(int c, FILE *stream);
 
-int puts(const char *s);
+extern int scanf(const char *format, ...);
+extern int fscanf(FILE *stream, const char *format, ...);
+extern int fgetc(FILE *stream);
+extern int putchar(int c);
+extern int fprintf(FILE *stream, const char *format, ...);
+extern int vfprintf(FILE *stream, const char *format, va_list ap);
 
-int open(const char *filename, int access, ...);
-int close(int fd);
-int unlink(const char *filename);
+extern int printf(const char *fmt, ...);
+extern int snprintf(char *s, size_t size, const char *fmt, ...);
+extern int vsnprintf(char *str, size_t size, const char *fmt, va_list va);
+extern int sprintf(char *s, const char *format, ...);
+extern int vsprintf(char *s, const char *format, va_list va);
+
+extern int puts(const char *s);
+
+extern int open(const char *filename, int access, ...);
+extern int close(int fd);
+extern int unlink(const char *filename);
 
 static inline int fileno(FILE *stream) { return stream->Handle; }
-int getc(FILE *stream);
+static inline int getc(FILE *stream) { return fgetc(stream); }
 #endif /* STDIO_H_ */
--- sources/getc.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-
-int getc(FILE *stream)
-{
-    return fgetc(stream);
-}
