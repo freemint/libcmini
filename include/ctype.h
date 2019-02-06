a3957ac5f380a5043a0319f4f8dcc45b26b5d5d7
Markus Fröschle <mfro@mubf.de>
2019-02-06 12:18:56 +0100

    add gcc ISxxx() macros that g++ requires

--- include/ctype.h
+++ include/ctype.h
@@ -8,6 +8,19 @@
 #ifndef _CTYPE_H_
 #define _CTYPE_H_
 
+extern unsigned int* _ctype;
+
+#define _IScntrl    0x01        /* control character */
+#define _ISdigit    0x02        /* numeric digit */
+#define _ISupper    0x04        /* upper case */
+#define _ISlower    0x08        /* lower case */
+#define _ISspace    0x10        /* whitespace */
+#define _ISpunct    0x20        /* punctuation */
+#define _ISxdigit   0x40        /* hexadecimal */
+#define _ISblank    0x80        /* blank */
+#define _ISgraph    0x100       /* graph */
+#define _ISprint    0x200       /* print */
+
 #define isdigit(c)	(((c) >= '0') && ((c) <= '9'))
 #define isupper(c)	((c) >= 'A' && ((c) <= 'Z'))
 #define islower(c)	((c) >= 'a' && ((c) <= 'z'))
