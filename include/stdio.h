2d9120b6953514d1ec11d511df9c607520af0277
rosenrost <oliver@graf-bussmeier.de>
2019-06-25 18:45:14 +0200

    Update stdio.h

--- include/stdio.h
+++ include/stdio.h
@@ -80,6 +80,7 @@ extern int errno;
 
 extern FILE *fopen(const char *path, const char *mode);
 extern int fclose(FILE *fp);
+extern int fcloseall();
 extern size_t fread(void *ptr, size_t size, size_t nmemb, FILE *stream);
 extern size_t fwrite(const void *ptr, size_t size, size_t nmemb, FILE *stream);
 extern int fseek(FILE *fp, long offset, int origin);
