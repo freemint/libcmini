9f4d612ba7ce3f5cc77bed72e77f65c5fb721e99
rosenrost <oliver@graf-bussmeier.de>
2019-06-25 18:01:15 +0200

    Update strdup.c
    Fixed wrong malloc size

--- sources/strdup.c
+++ sources/strdup.c
@@ -3,11 +3,11 @@
 
 char *strdup(const char *s1)
 {
-	char *dup;
+    char *dup;
 
-	dup = malloc(strlen(s1 + 1));
-	if (dup) strcpy(dup, s1);
+    dup = malloc(strlen(s1) + 1);
+    if (dup) strcpy(dup, s1);
 
-	return dup;
+    return dup;
 }
 
