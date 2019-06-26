ce9b0a3bb6a623ba9204cec7795c736e5cbbe980
rosenrost <oliver@graf-bussmeier.de>
2019-06-26 10:54:15 +0200

    New time functions and extension of mktime()

---
+++ sources/localtime.c
@@ -0,0 +1,105 @@
+#include <time.h>
+#include <osbind.h>
+
+#ifdef __MINTLIB_COMPATIBLE
+# include <sys/time.h>
+#endif /* __MINTLIB_COMPATIBLE */
+
+
+/* year range is from 1901 to 2038, so no century can occur which is not a leap year
+#define is_leap(y)  ((y) % 4 == 0 && ((y) % 100 != 0 || (y) % 400 == 0))
+*/
+#define is_leap(y)  ((y) % 4 == 0)
+
+
+const long _month_days[] = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
+enum { January, February, March, April, May, June, July, August, September, October, November, December, Months };
+enum { Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Weekdays };
+
+
+struct tm*
+localtime(const time_t* timep)
+{
+    static struct tm tm;
+
+    time_t t   = *timep;
+    int    neg = (t < 0);
+    int    step;
+    int    days;
+    int    seconds;
+    int    leap;
+    int    monthdays;
+
+    if (neg) {
+        t    = -t - 1;
+        step = -1;
+    } else {
+        step = 1;
+    }
+
+    days    = t / 86400;
+    seconds = t % 86400;
+
+    if (neg) {
+        tm.tm_wday = (Wednesday + days) % Weekdays;
+        tm.tm_year = 1969;
+        seconds    = 86399 - seconds;
+    } else {
+        tm.tm_wday = (Thursday + days) % Weekdays;
+        tm.tm_year = 1970;
+    }
+
+    for (;;) {
+        int yeardays = is_leap(tm.tm_year) ? 366 : 365;
+
+        if (neg) {
+            if (yeardays > days) {
+                tm.tm_yday = yeardays - days - 1;
+                break;
+            }
+        } else {
+            if (yeardays >= days) {
+                tm.tm_yday = days;
+                break;
+            }
+        }
+
+        tm.tm_year += step;
+        days       -= yeardays;
+    }
+
+    tm.tm_mon = neg ? 11 : 0;
+
+    leap = is_leap(tm.tm_year);
+
+    for (;;) {
+        monthdays = _month_days[tm.tm_mon];
+
+        if (leap && tm.tm_mon == 1) {
+            ++monthdays;
+        }
+
+        if (monthdays <= days) {
+            tm.tm_mon += step;
+            days      -= monthdays;
+
+            if (tm.tm_mon == 12) {
+                tm.tm_mon = 0;
+                ++tm.tm_year;
+            }
+        } else {
+            tm.tm_mday = neg ? monthdays : 1;
+            break;
+        }
+    }
+
+    tm.tm_year -= 1900;
+    tm.tm_mday += step * days;
+    tm.tm_hour  = seconds / 3600;
+    tm.tm_min   = (seconds % 3600) / 60;
+    tm.tm_sec   = seconds % 60;
+    tm.tm_isdst = -1;
+
+    return &tm;
+}
--- sources/mktime.c
+++ sources/mktime.c
@@ -3,21 +3,56 @@
 #	include <sys/time.h>
 #endif
 
+/* year range is from 1901 to 2038, so no century can occur which is not a leap year
+#define is_leap(y)  ((y) % 4 == 0 && ((y) % 100 != 0 || (y) % 400 == 0))
+*/
+#define is_leap(y)  ((y) % 4 == 0)
+
 time_t mktime(struct tm *tm)
 {
-	const int mon_days [] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-	long tyears, tdays, leaps, utc_hrs;
-	int i;
+    static const int mon_days[] =
+    {
+        31,
+        31 + 28,
+        31 + 28 + 31,
+        31 + 28 + 31 + 30,
+        31 + 28 + 31 + 30 + 31,
+        31 + 28 + 31 + 30 + 31 + 30,
+        31 + 28 + 31 + 30 + 31 + 30 + 31,
+        31 + 28 + 31 + 30 + 31 + 30 + 31 + 31,
+        31 + 28 + 31 + 30 + 31 + 30 + 31 + 31 + 30,
+        31 + 28 + 31 + 30 + 31 + 30 + 31 + 31 + 30 + 31,
+        31 + 28 + 31 + 30 + 31 + 30 + 31 + 31 + 30 + 31 + 30,
+        31 + 28 + 31 + 30 + 31 + 30 + 31 + 31 + 30 + 31 + 30 + 31
+    };
 
+	long tyears, tdays, leaps, utc_hrs;
+    int leap_year;
+
+    leap_year = is_leap(tm->tm_year + 1900);
 	tyears = tm->tm_year - 70;
 	leaps = (tyears + 2) / 4;
-	tdays = 0;
-	for (i=0; i < tm->tm_mon; i++)
-		tdays += mon_days[i];
 
-	tdays += tm->tm_mday-1;
-	tdays = tdays + (tyears * 365) + leaps;
+    if (tyears < 0) {
+        leaps = (tyears - 2) / 4;
 
-	utc_hrs = tm->tm_hour;
-	return (tdays * 86400) + (utc_hrs * 3600) + (tm->tm_min * 60) + tm->tm_sec;	
+        if (leap_year && tm->tm_mon > 1) {
+            ++leaps;
+        }
+    } else {
+        leaps = (tyears + 2) / 4; /* no of next two lines until year 2100. */
+
+        if (leap_year && (tm->tm_mon == 0 || tm->tm_mon == 1)) {
+            --leaps;
+        }
+    }
+
+    tdays = (tm->tm_mon > 0) ? mon_days[tm->tm_mon - 1] : 0;
+
+    tdays += tm->tm_mday - 1; /* days of month passed. */
+    tdays += (tyears * 365) + leaps;
+
+    utc_hrs = tm->tm_hour;
+
+    return (tdays * 86400) + (utc_hrs * 3600) + (tm->tm_min * 60) + tm->tm_sec;
 }
