789d9c2d4dddeac88b55fe8d6dbeb398f9ec6546
Thorsten Otto <admin@tho-otto.de>
2019-07-21 17:53:23 +0200

    Fix strchr

--- sources/strchr.c
+++ sources/strchr.c
@@ -1,16 +1,23 @@
 #include <string.h>
 
-char *strchr(const char *str, int c)
+#ifndef NO_CONST
+#  ifdef __GNUC__
+#    define NO_CONST(p) __extension__({ union { const void *cs; void *s; } x; x.cs = p; x.s; })
+#  else
+#    define NO_CONST(p) ((void *)(p))
+#  endif
+#endif
+
+char *strchr(const char *str, int charwanted)
 {
-    const char *p = str;
+	const char *p = str;
+	char c;
 
-    do
-    {
-        if (*p == (char)c)
-            return (char*)p;
-    } while (*p++);
+	do
+	{
+		if ((c = *p++) == (char) charwanted)
+			return NO_CONST(--p);
+	} while (c);
 
-    return NULL;
+	return NULL;
 }
-
-char *index(const char *str, int c) __attribute__((alias("strchr")));
