ea061b2d9b928e36fee9af47b8b164e6d2d07cba
Thorsten Otto <admin@tho-otto.de>
2019-07-21 16:53:21 +0200

    Replace strcmp() by version from mintlib

--- sources/strcmp.c
+++ sources/strcmp.c
@@ -7,15 +7,32 @@
 
 #include <string.h>
 
-int strcmp(const char *s1, const char *s2)
+#if defined(__GNUC__) && __GNUC__ >= 7
+#pragma GCC diagnostic ignored "-Wnonnull-compare"
+#endif
+
+int strcmp(const char *scan1, const char *scan2)
 {
-    int cmp;
+	unsigned char c1, c2;
 
-    do {
-        cmp = (unsigned char)*s1 - (unsigned char)*s2;
-    } while (cmp == 0 && *s1++ != '\0' && *s2++ != '\0');
+	if (!scan1)
+		return scan2 ? -1 : 0;
+	if (!scan2) return 1;
 
-    return cmp;
+	do {
+		c1 = (unsigned char) *scan1++; c2 = (unsigned char) *scan2++;
+	} while (c1 && c1 == c2);
+
+	/*
+	 * The following case analysis is necessary so that characters
+	 * which look negative collate low against normal characters but
+	 * high against the end-of-string NUL.
+	 */
+	if (c1 == c2)
+		return 0;
+	if (c1 == '\0')
+		return -1;
+	if (c2 == '\0')
+		return 1;
+	return c1 - c2;
 }
-
-
