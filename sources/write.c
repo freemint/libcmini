35f1c336eacb2a80e4fd65dc83d13e3af1bee952
Thorsten Otto <admin@tho-otto.de>
2019-07-23 17:26:05 +0200

    Set errno in write()

--- sources/write.c
+++ sources/write.c
@@ -1,10 +1,18 @@
 #include <osbind.h>
 #include <unistd.h>
-
+#include "lib.h"
 
 /* posix write needed by C++ in libstdc++-v3/libsupc++/pure.cc */
-ssize_t
-write(int __fd, __const void* __buf, size_t __n)
+ssize_t write(int fd, const void *buf, size_t nbytes)
 {
-    return Fwrite(__fd, __n, __buf);
+    long result;
+
+    result = Fwrite(fd, nbytes, buf);
+
+    if (result < 0)
+    {
+        __set_errno(-result);
+        return -1;
+    }
+    return result;
 }
