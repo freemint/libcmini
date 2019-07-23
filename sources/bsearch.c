ac4a322774dcbc468fa9251856d0ed5b20224f6e
Thorsten Otto <admin@tho-otto.de>
2019-07-24 01:56:18 +0200

    Simplify and reformat bsearch()

--- sources/bsearch.c
+++ sources/bsearch.c
@@ -1,51 +1,38 @@
 /* from Dale Schumacher's dLibs library */
 
-#include <stddef.h>
-#include <stdlib.h>
-#include <assert.h>
+#include "lib.h"
 
 /*
  * This routine is safe in the sense that it does not make
  * assumptions about sizeof(void *). Gcc assumes same as char *
  * when not -ansi, the "other" compiler just barfs.
  *
+ * don't fail when key is NULL; it might be some integer value casted to void *
  */
 
-
-void*
-bsearch(const void* key, const void* base, size_t num, size_t size, int (*cmp)(const void*, const void*))
+void *bsearch(const void *key,	/* item to search for */
+			  const void *base,	/* base address */
+			  size_t num,				/* number of elements */
+			  size_t size,		/* element size in bytes */
+			  int (*cmp)(const void *, const void *))	/* comparison function */
 {
-	register size_t a, b;
-
-#if 0
-	assert(key != NULL && base != NULL && size > 0 && num > 0 && cmp != NULL);
-#else
-	if (key == NULL || base == NULL || size == 0 || num == 0 || cmp == NULL) {
-		return NULL;
-	}
-#endif
+	size_t a, b, c;
+	int dir;
+	const void *p;
 
 	a = 0;
-	b = num - 1;
-
-	while (a <= b) {
-		register size_t c            = (a + b) / 2;
-		const void*     compare_with = (char*)base + c * size;
-		register int    dir          = cmp(key, compare_with);
-
-		if (dir == 0) {
-			return (void*)compare_with;
-		} else if (dir < 0) {
-			if (c == 0) {
-				return NULL;
-			}
-
-			b = c - 1;
-		} else  {
-			/* (dir > 0) */
+	b = num;
+	while (a < b)
+	{
+		c = (a + b) >> 1;				/* == ((a + b) / 2) */
+		p = (const void *) ((const char *) base + (c * size));
+		dir = (*cmp) (key, p);
+		if (dir < 0)
+			b = c;
+		else if (dir > 0)
 			a = c + 1;
-		}
+		else
+			return NO_CONST(p);
 	}
-
 	return NULL;
 }
