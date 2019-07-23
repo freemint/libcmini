23ed58a3247f924f77ed3e2de6a3982fa3a2fbf8
rosenrost <oliver@graf-bussmeier.de>
2019-07-23 20:13:08 +0200

    Issue #60: strcmp() and strncmp() compare characters unsigned

--- sources/strcmp.c
+++ sources/strcmp.c
@@ -12,7 +12,7 @@ int strcmp(const char *s1, const char *s2)
     int cmp;
 
     do {
-        cmp = *s1 - *s2;
+        cmp = (unsigned char)*s1 - (unsigned char)*s2;
     } while (cmp == 0 && *s1++ != '\0' && *s2++ != '\0');
 
     return cmp;
