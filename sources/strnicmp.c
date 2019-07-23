8a3197daaa0b3affed0d3e9ff053f31ce739273a
Thorsten Otto <admin@tho-otto.de>
2019-07-23 18:37:31 +0200

    Remove duplicate definiton of toupper form stricmp.c/strnicmp.c

--- sources/strnicmp.c
+++ sources/strnicmp.c
@@ -1,17 +1,19 @@
 #include <string.h>
-
-#define toupper(c) (((c) >= 'a' && (c) <= 'z') ? ((c) - ('a' - 'A')) : (c))
+#include <ctype.h>
 
 int strnicmp(const char *s1, const char *s2, size_t n)
 {
-	int c1, c2, d=0;
+	int c1, c2, d = 0;
 	for ( ; n > 0; --n,++s1, ++s2)
 	{
 		c1 = toupper(*s1);
 		c2 = toupper(*s2);
-		if (((d = c1 - c2) != 0) || (c2 == '\0'))
+		if (((d = c1 - c2) != 0) || c2 == '\0')
 			break;
 	}
 	return d;
 }
+
+#ifdef __GNUC__
 int strncasecmp(const char *s1, const char *s2, size_t n) __attribute__((alias("strnicmp")));
+#endif
