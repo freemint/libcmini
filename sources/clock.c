b4eb988c03b0df0da7d356c5d81e7b83b1440c34
Thorsten Otto <admin@tho-otto.de>
2019-07-24 01:43:56 +0200

    clock(): use Supexec rather than Super

--- sources/clock.c
+++ sources/clock.c
@@ -1,19 +1,17 @@
 #include <time.h>
 #include <mint/osbind.h>
 
-#define _hz_200                ((unsigned long *) 0x4baL)
+#define _hz_200                ((volatile unsigned long *) 0x4baL)
+
+
+static long get_clock(void)
+{
+	return *_hz_200;
+}
+
 
 clock_t clock(void)
 {
-	long ssp;
-	clock_t ret;
-
-	ssp = Super(0L);
-
-	ret = * (volatile clock_t *) _hz_200;
-
-	Super(ssp);
-
-	return ret;
+	return Supexec(get_clock);
 }
 
