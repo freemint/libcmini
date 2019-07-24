831222f8f52166b38bdd4529ac58d546c46982ba
Thorsten Otto <admin@tho-otto.de>
2019-07-24 03:48:37 +0200

    Replace strndup by mintlib version

--- sources/strndup.c
+++ sources/strndup.c
@@ -1,20 +1,18 @@
 #include <string.h>
 #include <stdlib.h>
 
-char *strndup(const char *s1, size_t n)
+char *strndup(const char *src, size_t n)
 {
-	char *dup;
-	size_t len;
-
-	len = strlen(s1);
-
-	if (len > n) len = n;
-	dup = malloc(len + 1);
-	
-	if (dup) {
-		memcpy(dup, s1, len); // use memcpy to suppress warnings
-		dup[len] = '\0'; // unlike strncpy, strndup always appends '\0'
-	}
-	return dup;
+  char* result;
+  size_t length = strlen (src);
+  if (length > n)
+    length = n;
+  
+  result = malloc (length + 1);
+  if (result == NULL)
+    return NULL;
+  
+  result[length] = '\0';
+  return memcpy (result, src, length);
 }
 
