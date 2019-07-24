2da9cb8e5c21e8814e0b20954baa01d9ad06b72f
Thorsten Otto <admin@tho-otto.de>
2019-07-24 03:31:58 +0200

    Set errno in fopen(), and fix memory leak

--- sources/fopen.c
+++ sources/fopen.c
@@ -7,115 +7,111 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <osbind.h>
+#include <mint/osbind.h>
+#include <errno.h>
 #include "lib.h"
 
 
-FILE* __stdio_head = NULL;
+FILE *__stdio_head = NULL;
 
 
-FILE*
-fopen(const char* path, const char* mode)
+FILE *fopen(const char *path, const char *mode)
 {
-    FILE* fp;
-    long  fd;
+	FILE *fp;
+	long fd;
+	int i;
 
-    if (mode == NULL || path == NULL) {
-        return NULL;
-    }
+	if (mode == NULL || path == NULL)
+	{
+		__set_errno(EFAULT);
+		return NULL;
+	}
 
-    fp = calloc(sizeof(FILE), 1);
+	if ((fp = calloc(sizeof(FILE), 1)) == NULL)
+		return NULL;
 
-    if (fp == NULL) {
-        return NULL;
-    } else {
-        register int i;
+	switch (*mode)
+	{
+	case 'a':
+		fp->__mode.__write = 1;
+		fp->__mode.__create = 1;
+		fp->__mode.__append = 1;
+		break;
 
-        switch (*mode) {
-            case 'a':
-                fp->__mode.__write  = 1;
-                fp->__mode.__create = 1;
-                fp->__mode.__append = 1;
-                break;
+	case 'w':
+		fp->__mode.__write = 1;
+		fp->__mode.__create = 1;
+		break;
 
-            case 'w':
-                fp->__mode.__write  = 1;
-                fp->__mode.__create = 1;
-                break;
+	case 'r':
+		fp->__mode.__read = 1;
+		break;
 
-            case 'r':
-                fp->__mode.__read = 1;
-                break;
+	default:
+		free(fp);
+		return NULL;
+	}
 
-            default:
-                goto error;
+	for (i = 1; i < 4; ++i)
+	{
+		switch (*++mode)
+		{
+		case '\0':
+		default:
+			break;
 
-        }
+		case '+':
+			fp->__mode.__read = 1;
+			fp->__mode.__write = 1;
+			continue;
 
-        for (i = 1; i < 4; ++i) {
-            switch (*++mode) {
-                case '\0':
-                default:
-                    break;
+		case 'b':
+			fp->__mode.__binary = 1;
+			continue;
 
-                case '+':
-                    fp->__mode.__read  = 1;
-                    fp->__mode.__write = 1;
-                    continue;
+		case 'x':
+			fp->__mode.__exclusive = 1;
+			continue;
+		}
 
-                case 'b':
-                    fp->__mode.__binary = 1;
-                    continue;
+		break;
+	}
 
-                case 'x':
-                    fp->__mode.__exclusive = 1;
-                    continue;
+	if (fp->__mode.__append) {
+		fd = Fopen(path, fp->__mode.__write ? 1 + fp->__mode.__read : 0);
 
-            }
+		if(fd >= 0) {
+			Fseek(0, fd, SEEK_END);
+			goto ok;
+		}
+	}
 
-            break;
-        }
+	if (fp->__mode.__create) {
+		fd = Fcreate (path, 0);
 
-        if (fp->__mode.__append) {
-            fd = Fopen(path, fp->__mode.__write ? 1 + fp->__mode.__read : 0);
+		if (fd < 0)
+		{
+			__set_errno(-fd);
+			free(fp);
+			return NULL;
+		}
+		if (fp->__mode.__read)
+			goto ok;
+		Fclose(fd);
+	}
 
-            if(fd >= 0) {
-                Fseek(0, fd, SEEK_END);
-                goto ok;
-            }
-        }
-
-        if (fp->__mode.__create) {
-            fd = Fcreate (path, 0);
-
-            if (fd < 0) {
-                goto error;
-            } else if (fp->__mode.__read) {
-                goto ok;
-            }
-
-            Fclose(fd);
-        }
-
-        fd = Fopen(path, fp->__mode.__write ? 1 + fp->__mode.__read : 0);
-
-        if (fd >= 0) {
-            goto ok;
-        }
-    }
-
-error:
-    free(fp);
-    return NULL;
+	if ((fd = Fopen(path, fp->__mode.__write ? 1 + fp->__mode.__read : 0)) >= 0)
+		goto ok;
+	__set_errno(-fd);
+	free(fp);
+	return NULL;
 
 ok:
-    fp->__magic = _IOMAGIC;
-    FILE_SET_HANDLE(fp, fd);
-    fp->__pushback = EOF;
-    fp->__next = __stdio_head;
-    fp->__eof = 0;
-    fp->__error = 0;
-    __stdio_head = fp;
+	fp->__magic = _IOMAGIC;
+	FILE_SET_HANDLE(fp, fd);
+	fp->__pushback = EOF;
+	fp->__next = __stdio_head;
+	__stdio_head = fp;
 
-    return fp;
+	return fp;
 }
