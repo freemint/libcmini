438fbc4c2561a8945f361ec30d117ac8f7ef7d48
Thorsten Otto <admin@tho-otto.de>
2020-04-26 19:12:02 +0200

    Simplify stpcpy()/stpncpy()

--- sources/stpcpy.c
+++ sources/stpcpy.c
@@ -23,15 +23,9 @@
 #undef stpcpy
 
 /* Copy SRC to DEST, returning the address of the terminating '\0' in DEST.  */
-char*
-stpcpy(char* dest, const char* src)
+char *stpcpy(char *dest, const char *src)
 {
-  register char *d = dest;
-  register const char *s = src;
-
-  do
-    *d++ = *s;
-  while (*s++ != '\0');
-
-  return d - 1;
+	while ((*dest++ = *src++) != '\0')
+		;
+	return dest - 1;
 }
--- sources/stpncpy.c
+++ sources/stpncpy.c
@@ -23,60 +23,26 @@
 
 /* Copy no more than N characters of SRC to DEST, returning the address of
    the terminating '\0' in DEST, if any, or else DEST + N.  */
-char*
-stpncpy(char* dest, const char* src, size_t n)
+char *stpncpy(char *dest, const char *src, size_t n)
 {
-  char c;
-  char *s = dest;
+	char c;
 
-  if (n >= 4)
-    {
-      size_t n4 = n >> 2;
+	if (n == 0)
+		return dest;
 
-      for (;;)
-    {
-      c = *src++;
-      *dest++ = c;
-      if (c == '\0')
-        break;
-      c = *src++;
-      *dest++ = c;
-      if (c == '\0')
-        break;
-      c = *src++;
-      *dest++ = c;
-      if (c == '\0')
-        break;
-      c = *src++;
-      *dest++ = c;
-      if (c == '\0')
-        break;
-      if (--n4 == 0)
-        goto last_chars;
-    }
-      n -= dest - s;
-      goto zero_fill;
-    }
+	for (;;)
+	{
+		c = *src++;
+		--n;
+		*dest++ = c;
+		if (c == '\0')
+			break;
+		if (n == 0)
+			return dest;
+	}
 
- last_chars:
-  n &= 3;
-  if (n == 0)
-    return dest;
+	while (n-- > 0)
+		dest[n] = '\0';
 
-  for (;;)
-    {
-      c = *src++;
-      --n;
-      *dest++ = c;
-      if (c == '\0')
-    break;
-      if (n == 0)
-    return dest;
-    }
-
- zero_fill:
-  while (n-- > 0)
-    dest[n] = '\0';
-
-  return dest - 1;
+	return dest - 1;
 }
