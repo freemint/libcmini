855311fe4c6b79ee64e3957dd4a25f0a5a6a2ee7
Thorsten Otto <admin@tho-otto.de>
2020-04-26 13:07:06 +0200

    Make ldiv an alias for div if possible

--- sources/ldiv.c
+++ sources/ldiv.c
@@ -1,20 +1,27 @@
-/*
- * ldiv.c
- *
- *  Created on: 12.07.2017
- *      Author: og
- */
+/*  div.c -- MiNTLib.
+    Copyright (C) 2000 Frank Naumann <fnaumann@freemint.de>
 
+    This file is part of the MiNTLib project, and may only be used
+    modified and distributed under the terms of the MiNTLib project
+    license, COPYMINT.  By continuing to use, modify, or distribute
+    this file you indicate that you have read the license and
+    understand and accept it fully.
+*/
+
+/* Modified by Guido Flohr <guido@freemint.de>:
+   - Make ldiv a strong alias for div when not compiling mshort.  */
+   
+#include "lib.h"
 #include <stdlib.h>
 
-
-ldiv_t
-ldiv(long n, long d)
+#ifdef __MSHORT__
+ldiv_t ldiv(long num, long denom)
 {
-	ldiv_t r;
-
-	r.quot = n / d;
-	r.rem  = n % d;
-
-	return r;
+	ldiv_t res;
+	
+	res.quot = num / denom;
+	res.rem = num % denom;
+	
+	return res;
 }
+#endif
