45a5cf2b31be36edb00663e1caa0a5c5b3cf0bae
Thorsten Otto <admin@tho-otto.de>
2020-04-26 13:14:26 +0200

    fwrite: reformat

--- sources/fwrite.c
+++ sources/fwrite.c
@@ -4,18 +4,19 @@
 #include <sys/types.h>
 #include "lib.h"
 
-
-size_t
-fwrite(const void* ptr, size_t size, size_t nmemb, FILE* stream)
+size_t fwrite(const void *ptr, size_t size, size_t nmemb, FILE *stream)
 {
-    long rc = Fwrite(FILE_GET_HANDLE(stream), size * nmemb, ptr);
+	long rc = Fwrite(FILE_GET_HANDLE(stream), size * nmemb, ptr);
 
-    if (rc < 0) {
-        stream->__error = 1;
-        rc = 0;
-    } else {
-        rc /= size;
-    }
+	if (rc < 0)
+	{
+		stream->__error = 1;
+		__set_errno(-rc);
+		rc = 0;
+	} else
+	{
+		rc /= size;
+	}
 
-    return rc;
+	return rc;
 }
