d53aaae7bc44045cf3c070dd61b0e02f2e65ec4c
Thorsten Otto <admin@tho-otto.de>
2020-04-26 18:11:54 +0200

    qsort: reformat

--- sources/qsort.c
+++ sources/qsort.c
@@ -24,8 +24,8 @@
 #define SWAP(a, b, size)                              \
   do                                          \
     {                                         \
-      register size_t __size = (size);                        \
-      register char *__a = (a), *__b = (b);                   \
+      size_t __size = (size);                        \
+      char *__a = (a), *__b = (b);                   \
       do                                      \
     {                                     \
       char __tmp = *__a;                              \
@@ -40,10 +40,10 @@
 
 /* Stack node declarations used to store unfulfilled partition obligations. */
 typedef struct
-  {
-    char *lo;
-    char *hi;
-  } stack_node;
+{
+	char *lo;
+	char *hi;
+} stack_node;
 
 /* The next 4 #defines implement a very fast in-line stack abstraction. */
 #define STACK_SIZE  (8 * sizeof(unsigned long int))
@@ -75,177 +75,166 @@ typedef struct
       smaller partition.  This *guarantees* no more than log (n)
       stack size is needed (actually O(1) in this case)!  */
 
-void
-qsort(void *base, size_t nel, size_t width, int (*compar)(const void *, const void *))
+void qsort(void *base, size_t nel, size_t width, int (*compar)(const void *, const void *))
 {
-  register char *base_ptr = (char *) base;
+	char *base_ptr = (char *) base;
 
-  /* Allocating SIZE bytes for a pivot buffer facilitates a better
-     algorithm below since we can do comparisons directly on the pivot. */
-#ifdef MINTLIB_COMPATIBLE
-  char *pivot_buffer = (char *) __alloca (width);
-#else
-  char *pivot_buffer = malloc (width);
-#endif /* MINTLIB_COMPATIBLE */
-  const size_t max_thresh = MAX_THRESH * width;
+	/* Allocating SIZE bytes for a pivot buffer facilitates a better
+	   algorithm below since we can do comparisons directly on the pivot. */
+	char *pivot_buffer;
+	const size_t max_thresh = MAX_THRESH * width;
 
-  if (nel == 0) {
-    /* Avoid lossage with unsigned arithmetic below.  */
-#ifndef MINTLIB_COMPATIBLE
-    free(pivot_buffer);
-#endif /* MINTLIB_COMPATIBLE */
-    return;
-  }
+	if (nel == 0)
+	{
+		/* Avoid lossage with unsigned arithmetic below.  */
+		return;
+	}
+	pivot_buffer = malloc(width);
 
-  if (nel > MAX_THRESH)
-    {
-      char *lo = base_ptr;
-      char *hi = &lo[width * (nel - 1)];
-      /* Largest size needed for 32-bit int!!! */
-      stack_node stack[STACK_SIZE];
-      stack_node *top = stack + 1;
+	if (nel > MAX_THRESH)
+	{
+		char *lo = base_ptr;
+		char *hi = &lo[width * (nel - 1)];
 
-      while (STACK_NOT_EMPTY)
-        {
-          char *left_ptr;
-          char *right_ptr;
+		/* Largest size needed for 32-bit int!!! */
+		stack_node stack[STACK_SIZE];
+		stack_node *top = stack + 1;
 
-      char *pivot = pivot_buffer;
+		while (STACK_NOT_EMPTY)
+		{
+			char *left_ptr;
+			char *right_ptr;
 
-      /* Select median value from among LO, MID, and HI. Rearrange
-         LO and HI so the three values are sorted. This lowers the
-         probability of picking a pathological pivot value and
-         skips a comparison for both the LEFT_PTR and RIGHT_PTR. */
+			char *pivot = pivot_buffer;
 
-      char *mid = lo + width * ((hi - lo) / width >> 1);
+			/* Select median value from among LO, MID, and HI. Rearrange
+			   LO and HI so the three values are sorted. This lowers the
+			   probability of picking a pathological pivot value and
+			   skips a comparison for both the LEFT_PTR and RIGHT_PTR. */
 
-      if ((*compar) ((void *) mid, (void *) lo) < 0)
-        SWAP (mid, lo, width);
-      if ((*compar) ((void *) hi, (void *) mid) < 0)
-        SWAP (mid, hi, width);
-      else
-        goto jump_over;
-      if ((*compar) ((void *) mid, (void *) lo) < 0)
-        SWAP (mid, lo, width);
-    jump_over:
-      memcpy (pivot, mid, width);
-      pivot = pivot_buffer;
+			char *mid = lo + width * ((hi - lo) / width >> 1);
 
-      left_ptr  = lo + width;
-      right_ptr = hi - width;
+			if ((*compar) ((void *) mid, (void *) lo) < 0)
+				SWAP(mid, lo, width);
+			if ((*compar) ((void *) hi, (void *) mid) < 0)
+				SWAP(mid, hi, width);
+			else
+				goto jump_over;
+			if ((*compar) ((void *) mid, (void *) lo) < 0)
+				SWAP(mid, lo, width);
+		  jump_over:
+			memcpy(pivot, mid, width);
+			pivot = pivot_buffer;
 
-      /* Here's the famous ``collapse the walls'' section of quicksort.
-         Gotta like those tight inner loops!  They are the main reason
-         that this algorithm runs much faster than others. */
-      do
-        {
-          while ((*compar) ((void *) left_ptr, (void *) pivot) < 0)
-        left_ptr += width;
+			left_ptr = lo + width;
+			right_ptr = hi - width;
 
-          while ((*compar) ((void *) pivot, (void *) right_ptr) < 0)
-        right_ptr -= width;
+			/* Here's the famous ``collapse the walls'' section of quicksort.
+			   Gotta like those tight inner loops!  They are the main reason
+			   that this algorithm runs much faster than others. */
+			do
+			{
+				while ((*compar) ((void *) left_ptr, (void *) pivot) < 0)
+					left_ptr += width;
 
-          if (left_ptr < right_ptr)
-        {
-          SWAP (left_ptr, right_ptr, width);
-          left_ptr += width;
-          right_ptr -= width;
-        }
-          else if (left_ptr == right_ptr)
-        {
-          left_ptr += width;
-          right_ptr -= width;
-          break;
-        }
-        }
-      while (left_ptr <= right_ptr);
+				while ((*compar) ((void *) pivot, (void *) right_ptr) < 0)
+					right_ptr -= width;
 
-          /* Set up pointers for next iteration.  First determine whether
-             left and right partitions are below the threshold size.  If so,
-             ignore one or both.  Otherwise, push the larger partition's
-             bounds on the stack and continue sorting the smaller one. */
+				if (left_ptr < right_ptr)
+				{
+					SWAP(left_ptr, right_ptr, width);
+					left_ptr += width;
+					right_ptr -= width;
+				} else if (left_ptr == right_ptr)
+				{
+					left_ptr += width;
+					right_ptr -= width;
+					break;
+				}
+			} while (left_ptr <= right_ptr);
 
-          if ((size_t) (right_ptr - lo) <= max_thresh)
-            {
-              if ((size_t) (hi - left_ptr) <= max_thresh)
-        /* Ignore both small partitions. */
-                POP (lo, hi);
-              else
-        /* Ignore small left partition. */
-                lo = left_ptr;
-            }
-          else if ((size_t) (hi - left_ptr) <= max_thresh)
-        /* Ignore small right partition. */
-            hi = right_ptr;
-          else if ((right_ptr - lo) > (hi - left_ptr))
-            {
-          /* Push larger left partition indices. */
-              PUSH (lo, right_ptr);
-              lo = left_ptr;
-            }
-          else
-            {
-          /* Push larger right partition indices. */
-              PUSH (left_ptr, hi);
-              hi = right_ptr;
-            }
-        }
-    }
+			/* Set up pointers for next iteration.  First determine whether
+			   left and right partitions are below the threshold size.  If so,
+			   ignore one or both.  Otherwise, push the larger partition's
+			   bounds on the stack and continue sorting the smaller one. */
 
-  /* Once the BASE_PTR array is partially sorted by quicksort the rest
-     is completely sorted using insertion sort, since this is efficient
-     for partitions below MAX_THRESH size. BASE_PTR points to the beginning
-     of the array to sort, and END_PTR points at the very last element in
-     the array (*not* one beyond it!). */
+			if ((size_t) (right_ptr - lo) <= max_thresh)
+			{
+				if ((size_t) (hi - left_ptr) <= max_thresh)
+					/* Ignore both small partitions. */
+					POP(lo, hi);
+				else
+					/* Ignore small left partition. */
+					lo = left_ptr;
+			} else if ((size_t) (hi - left_ptr) <= max_thresh)
+				/* Ignore small right partition. */
+				hi = right_ptr;
+			else if ((right_ptr - lo) > (hi - left_ptr))
+			{
+				/* Push larger left partition indices. */
+				PUSH(lo, right_ptr);
+				lo = left_ptr;
+			} else
+			{
+				/* Push larger right partition indices. */
+				PUSH(left_ptr, hi);
+				hi = right_ptr;
+			}
+		}
+	}
+
+	/* Once the BASE_PTR array is partially sorted by quicksort the rest
+	   is completely sorted using insertion sort, since this is efficient
+	   for partitions below MAX_THRESH size. BASE_PTR points to the beginning
+	   of the array to sort, and END_PTR points at the very last element in
+	   the array (*not* one beyond it!). */
 
 #define min(x, y) ((x) < (y) ? (x) : (y))
 
-  {
-    char *const end_ptr = &base_ptr[width * (nel - 1)];
-    char *tmp_ptr = base_ptr;
-    char *thresh = min(end_ptr, base_ptr + max_thresh);
-    register char *run_ptr;
+	{
+		char *const end_ptr = &base_ptr[width * (nel - 1)];
+		char *tmp_ptr = base_ptr;
+		char *thresh = min(end_ptr, base_ptr + max_thresh);
+		char *run_ptr;
 
-    /* Find smallest element in first threshold and place it at the
-       array's beginning.  This is the smallest array element,
-       and the operation speeds up insertion sort's inner loop. */
+		/* Find smallest element in first threshold and place it at the
+		   array's beginning.  This is the smallest array element,
+		   and the operation speeds up insertion sort's inner loop. */
 
-    for (run_ptr = tmp_ptr + width; run_ptr <= thresh; run_ptr += width)
-      if ((*compar) ((void *) run_ptr, (void *) tmp_ptr) < 0)
-        tmp_ptr = run_ptr;
+		for (run_ptr = tmp_ptr + width; run_ptr <= thresh; run_ptr += width)
+			if ((*compar) ((void *) run_ptr, (void *) tmp_ptr) < 0)
+				tmp_ptr = run_ptr;
 
-    if (tmp_ptr != base_ptr)
-      SWAP (tmp_ptr, base_ptr, width);
+		if (tmp_ptr != base_ptr)
+			SWAP(tmp_ptr, base_ptr, width);
 
-    /* Insertion sort, running from left-hand-side up to right-hand-side.  */
+		/* Insertion sort, running from left-hand-side up to right-hand-side.  */
 
-    run_ptr = base_ptr + width;
-    while ((run_ptr += width) <= end_ptr)
-      {
-    tmp_ptr = run_ptr - width;
-    while ((*compar) ((void *) run_ptr, (void *) tmp_ptr) < 0)
-      tmp_ptr -= width;
+		run_ptr = base_ptr + width;
+		while ((run_ptr += width) <= end_ptr)
+		{
+			tmp_ptr = run_ptr - width;
+			while ((*compar) ((void *) run_ptr, (void *) tmp_ptr) < 0)
+				tmp_ptr -= width;
 
-    tmp_ptr += width;
-        if (tmp_ptr != run_ptr)
-          {
-            char *trav;
+			tmp_ptr += width;
+			if (tmp_ptr != run_ptr)
+			{
+				char *trav;
 
-        trav = run_ptr + width;
-        while (--trav >= run_ptr)
-              {
-                char c = *trav;
-                char *hi, *lo;
+				trav = run_ptr + width;
+				while (--trav >= run_ptr)
+				{
+					char c = *trav;
+					char *hi, *lo;
 
-                for (hi = lo = trav; (lo -= width) >= tmp_ptr; hi = lo)
-                  *hi = *lo;
-                *hi = c;
-              }
-          }
-      }
-  }
+					for (hi = lo = trav; (lo -= width) >= tmp_ptr; hi = lo)
+						*hi = *lo;
+					*hi = c;
+				}
+			}
+		}
+	}
 
-#ifndef MINTLIB_COMPATIBLE
-  free(pivot_buffer);
-#endif /* MINTLIB_COMPATIBLE */
+	free(pivot_buffer);
 }
