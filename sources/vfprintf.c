8fb0e5d5469cc5c8c8787dd02e19acb098dead65
rosenrost <oliver@graf-bussmeier.de>
2020-09-13 16:01:43 +0200

    Remove newline mapping

--- sources/vfprintf.c
+++ sources/vfprintf.c
@@ -2,33 +2,14 @@
 #include <stdarg.h>
 #include "lib.h"
 
-static int last_char;
 
 static int fpc(int c, void *fp)
 {
-	int   ret    = '\0';
-	FILE* stream = fp;
-
-	if (!stream->__mode.__binary) {
-		if (c == '\n' && last_char != '\r') {
-			if (putc('\r', stream) == EOF) {
-				ret = EOF;
-			}
-		}
-
-		last_char = c;
-	}
-
-	if (ret != EOF) {
-		ret = putc(c, stream);
-	}
-
-	return ret;
+    FILE* stream = fp;
+    return (putc(c, stream) == EOF) ? 0 : 1;
 }
 
 int vfprintf(FILE *stream, const char *format, va_list ap)
 {
-	return doprnt(fpc, stream, format, ap);
+    return doprnt(fpc, stream, format, ap);
 }
-
-
