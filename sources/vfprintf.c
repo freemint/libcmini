9b44f6e3bcad16f04c1ac90a61596815aeccbeab
rosenrost <oliver@graf-bussmeier.de>
2020-09-13 15:24:41 +0200

    Fix newline mapping for text files in fpc()
    Changes for new fwrite() newline mapping

--- sources/vfprintf.c
+++ sources/vfprintf.c
@@ -2,18 +2,21 @@
 #include <stdarg.h>
 #include "lib.h"
 
+#ifdef STDIO_MAP_NEWLINE
 static int last_char;
+#endif /* defined STDIO_MAP_NEWLINE */
 
 static int fpc(int c, void *fp)
 {
-	int   ret    = '\0';
 	FILE* stream = fp;
 
+#ifdef STDIO_MAP_NEWLINE
+    int ret = '\0';
+
 	if (!stream->__mode.__binary) {
-		if (c == '\n' && last_char != '\r') {
-			if (putc('\r', stream) == EOF) {
-				ret = EOF;
-			}
+		if (c == '\n' && last_char == '\r') {
+            last_char = c;
+            return 0;
 		}
 
 		last_char = c;
@@ -24,10 +27,17 @@ static int fpc(int c, void *fp)
 	}
 
 	return ret;
+#else
+    return putc(c, stream);
+#endif /* defined STDIO_MAP_NEWLINE */
 }
 
 int vfprintf(FILE *stream, const char *format, va_list ap)
 {
+#ifdef STDIO_MAP_NEWLINE
+    last_char = '\0';
+#endif /* defined STDIO_MAP_NEWLINE */
+
 	return doprnt(fpc, stream, format, ap);
 }
 
