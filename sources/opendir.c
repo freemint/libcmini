5bca94db892d00373481d31f413edefd2887125b
rosenrost <oliver@graf-bussmeier.de>
2020-09-20 15:54:53 +0200

    Remove setting errno to ENOMEM if memory allocation fails
    malloc() does it itself.

--- sources/opendir.c
+++ sources/opendir.c
@@ -39,11 +39,10 @@ DIR *opendir(const char *uname)
 
     strcpy(namebuf, uname);
     name = namebuf;
-    
+
     d = malloc(sizeof(DIR));
     if (!d)
     {
-        __set_errno(ENOMEM);
         return NULL;
     }
 
