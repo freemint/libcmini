5a71b34fc52227c404b8fcb11657fb5ab6694f3d
rosenrost <52100845+rosenrost@users.noreply.github.com>
2026-03-13 14:24:45 +0100

    Add error handling
    Error handling has been added; `errno` is set in case of an error. For example, `fclose(NULL)` now correctly returns `EOF` instead of `0`.

--- sources/fclose.c
+++ sources/fclose.c
@@ -7,6 +7,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <errno.h>
 #include <mint/osbind.h>
 #include "lib.h"
 
@@ -15,20 +16,34 @@ int fclose(FILE *fp)
 {
 	if (fp && fp->__magic == _IOMAGIC)
 	{
-		FILE **prev;
-		
-		Fclose(FILE_GET_HANDLE(fp));
-		FILE_SET_HANDLE(fp, -7);
-		fp->__magic = 0;
+        int r;
 
-		prev = &__stdio_head;
-		for (prev = &__stdio_head; (*prev) && *prev != fp; prev = &(*prev)->__next)
-			;
-		if (*prev == fp)
-		{
-			*prev = fp->__next;
-			free(fp);
-		}
-	}
+        r = Fclose(FILE_GET_HANDLE(fp));
+        if (r < 0) {
+            __set_errno (-r);
+            return EOF;
+        } else
+        {
+            FILE **prev;
+
+            FILE_SET_HANDLE(fp, -7);
+            fp->__magic = 0;
+
+            prev = &__stdio_head;
+            for (prev = &__stdio_head; (*prev) && *prev != fp; prev = &(*prev)->__next)
+                ;
+            if (*prev == fp)
+            {
+                *prev = fp->__next;
+                free(fp);
+            }
+
+            return 0;
+        }
+	} else
+    {
+        __set_errno (EINVAL);
+        return EOF;
+    }
 	return 0;
 }
